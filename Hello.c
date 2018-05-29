82ed74e1b40fce23b4911c9aa7d9cd4b538460b5
pspfriends1 <39721259+pspfriends1@users.noreply.github.com>
2018-05-29 12:25:14 +0530

    Updated as per psp1

--- Hello.c
+++ Hello.c
@@ -2,10 +2,12 @@
 int main()
 {
    char Name[30];
+   
    // printf() displays the string inside quotation
    printf("Hello, World!");
    printf("Enter Ur Name");
    scanf("%s",Name);
-   printf("So its  %s then",Name);
+   printf("So its  %s then\n",Name);
+   prinf("Some Changes Finally");
    return 0;
 }
