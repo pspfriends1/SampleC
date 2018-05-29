ee05781eb639a39e0d2b7649109a08569aae6156
pspfriends1 <39721259+pspfriends1@users.noreply.github.com>
2018-05-29 11:19:07 +0530

    input String

--- Hello.c
+++ Hello.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 int main()
 {
+   char Name[30];
    // printf() displays the string inside quotation
    printf("Hello, World!");
+   printf("Enter Ur Name");
+   scanf("%s",Name);
    return 0;
 }
